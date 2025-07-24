4ac3af42191bbeb096df2492d98d08efd2b8b9b5
SK sanoyar ali <asksanoyar33@gmail.com>
2025-07-25 00:19:27 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 // const heading=React.createElement("h1",{id:"heading"},"Hello world from react");
 // console.log(heading);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+        [React.createElement("h1",{},"I am h1 taghljlhs,hk"),React.createElement("h2",{},"I am h2 tag")]
     ),
    React.createElement("div",{id:"child2"},
-    [React.createElement("h3",{},"i am h3"),React.createElement("p",{},"i am p tag")]
+    [React.createElement("h3",{},"i am hskhwsh3"),React.createElement("p",{},"i am p tag")]
    )]
 );
 console.log(parent);
